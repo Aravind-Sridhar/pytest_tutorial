6c2ccf627d45cdd134069388f81ac3b8d26b6412
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2025-07-11 17:49:24 +0530

    Class testing started

---
+++ tests/tests_circle.py
@@ -0,0 +1,15 @@
+import pytest
+import source.shapes as shapes
+import math
+
+class TestCircle:
+    
+    def setup_method(self, method):
+        print(f'Setting up {method}')
+        self.circle = shapes.Circle(10)
+
+    def teardown_method(self, method):
+        print(f'Tearing down {method}')
+
+    def test_area(self):
+        assert self.circle.area() == math.pi * (self.circle.radius) **2
