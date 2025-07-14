d166ee2808aeafd4df3958778972425734fff8cd
Aravind Sridhar <91729968+Aravind-Sridhar@users.noreply.github.com>
2025-07-14 15:07:08 +0530

    Class testing done

--- tests/tests_circle.py
+++ tests/tests_circle.py
@@ -13,3 +13,6 @@ class TestCircle:
 
     def test_area(self):
         assert self.circle.area() == math.pi * (self.circle.radius) **2
+
+    def test_perimeter(self):
+        assert self.circle.perimeter() == 2 * math.pi * (self.circle.radius)
